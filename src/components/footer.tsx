df6f02cc13c4d2afe8c3bfac6fc37f7deba54d48
Andrea Hubacek <hubacekk@gmail.com>
2023-05-14 02:29:19 -0300

    footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,17 @@
-export default function Footer(){
-    return(
-        <footer>
-            <p>Creado por Andrea Hubacek</p>
 
+import { url } from "inspector"
+import styles from "../styles/footer.module.scss"
+import linkedin from "../assets/linkedin.png"
+import site from "../assets/sitio-web.png"
+
+export default function Footer() {
+   
+    return(
+        <footer className={styles.footer}>
+            
+            <span className={styles.creator}>Creado por Andrea Hubacek</span>
+            <a href="https://www.linkedin.com/in/andreahubacek/" target="_blank"> <img className={styles.icon} src={linkedin.src} /></a>
+            <a href="https://andreahubacek.vercel.app/" target="_blank"><img className={styles.icon} src={site.src}/></a>
        </footer>
     )
 }
